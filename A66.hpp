62e4070f63dad02baa0364b043cdcc24afd7ebdc
Adam Walder <walder2@llnl.gov>
2022-10-10 17:53:20 -0700

    commit 1

--- A66.hpp
+++ A66.hpp
@@ -1,7 +1,33 @@
-//
+
 
 #include <iomanip>
 #include <iostream>
 using namespace std;
 
 // complete the function maxmin( )
+void maxmin(int, int, int, int &, int &);
+
+void maxmin(int num1, int num2, int num3, int &maxVal, int &minVal)
+{
+
+    
+    if((num1 > num2) && (num1 > num3))
+        maxVal = num1;
+    
+    else if((num2 > num1) && (num2 > num3))
+        maxVal = num2;
+
+    else if((num3 > num1) && (num3 > num2))
+        maxVal = num3;
+    
+
+    if((num1 < num2) && (num1 < num3))
+        minVal = num1;
+    
+    else if((num2 < num1) && (num2 < num3))
+        minVal = num2;
+
+    else if((num3 < num1) && (num3 < num2))
+        minVal = num3;
+    
+}
